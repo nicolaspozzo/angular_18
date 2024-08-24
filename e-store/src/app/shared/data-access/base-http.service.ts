41662f0fd2c26c86a236a9b224a20f075eecf93b
unknown <nicolaspozzo19@gmail.com>
2024-08-24 18:03:26 -0300

    feat: create base-http service

---
+++ e-store/src/app/shared/data-access/base-http.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseHttpService {
+
+  public http = inject(HttpClient);
+  apiUrl = environment.API_URL;
+
+}
