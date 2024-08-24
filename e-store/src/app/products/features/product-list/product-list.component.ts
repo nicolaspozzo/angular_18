d143792ae8cb0e46b199a9197c5e06545a145f72
unknown <nicolaspozzo19@gmail.com>
2024-08-24 18:04:15 -0300

    feat: get products

--- e-store/src/app/products/features/product-list/product-list.component.ts
+++ e-store/src/app/products/features/product-list/product-list.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductStateService } from '../data-access/product-state.service';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [],
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  providers: [ProductStateService]
 })
 export default class ProductListComponent {
+
+  productsState = inject(ProductStateService)
+
 
 }
